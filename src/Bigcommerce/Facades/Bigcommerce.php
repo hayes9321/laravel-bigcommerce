36269c539284406ac561164f8a127ca556b9137e
Travis Hayes <>
2021-03-14 08:31:14 -0700

    Refactor

--- src/Bigcommerce/Facades/Bigcommerce.php
+++ src/Bigcommerce/Facades/Bigcommerce.php
@@ -4,8 +4,9 @@ namespace Oseintow\Bigcommerce\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-class Bigcommerce extends Facade {
-
-    protected static function getFacadeAccessor() { return 'bigcommerce'; }
-
+class Bigcommerce extends Facade
+{
+    protected static function getFacadeAccessor() {
+        return 'bigcommerce';
+    }
 }
